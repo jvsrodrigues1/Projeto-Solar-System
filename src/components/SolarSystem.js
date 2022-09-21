8256b60a538738fdbbb8fbb8f103a4d6166755bd
Joao Victor <imashinzo10@gmail.com>
2022-09-21 15:58:12 -0300

    Requisito 2 finalizado

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div className='data-testid="solar-system"' />
+    );
+  }
+}
+
+export default SolarSystem;
